f86f1e1a1d222e48992eb774950a0cdfc2704202
Rob Matwiejczyk <rob@parall.ax>
2015-12-17 17:22:37 +0800

    check for no files & add gulp-util

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var glob = require('glob'),
     gulp = require('gulp'),
+    gutil = require('gulp-util'),
     gulpicon = require('gulpicon/tasks/gulpicon'),
     elixir = require('laravel-elixir'),
     config = elixir.config;
@@ -13,13 +14,16 @@ elixir.extend('gulpicon', function(src, output, options) {
     };
 
     var src = (src || config.get('assets.svg.folder')) + '/**/*.svg',
-        gulpicon_config = options || require("../gulpicon/example/config.js");
+        gulpicon_config = options || require("gulpicon/example/config.js");
 
     gulpicon_config.dest = output || config.get('public.svg.outputFolder');
 
     new elixir.Task('gulpicon', function(){
         var files = glob.sync(src);
-        (gulpicon(files, gulpicon_config))();
+        if (files.length > 0) {
+            (gulpicon(files, gulpicon_config))();
+            gutil.log('Finished gulpicon conversion');
+        }
     }).watch([
         src
     ]);
